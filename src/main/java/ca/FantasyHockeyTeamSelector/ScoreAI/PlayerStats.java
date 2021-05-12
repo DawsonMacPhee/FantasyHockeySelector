94d1d70f34e74f82818322b23dee6dfc9413242e
Dawson MacPhee <dmacphee@uoguelph.ca>
2021-05-11 20:04:24 -0400

    Uploading work already created

---
+++ src/main/java/ca/FantasyHockeyTeamSelector/ScoreAI/PlayerStats.java
@@ -0,0 +1,21 @@
+package ca.FantasyHockeyTeamSelector.ScoreAI;
+
+public class PlayerStats {
+    public Long assists;
+    public Long blocked;
+    public Double faceOffPct;
+    public Long games;
+    public Long goals;
+    public Long hits;
+    public Long pim;
+    public Long plusMinus;
+    public Long points;
+    public String powerPlayTimeOnIce;
+    public String powerPlayTimeOnIcePerGame;
+    public Double shotPct;
+    public Long shots;
+    public String timeOnIce;
+    public String timeOnIcePerGame;
+
+    public Long yearStatScore = 0L;
+}
