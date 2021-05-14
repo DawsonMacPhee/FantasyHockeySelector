5f2d82d1ae51c91ddcd3cc44ab66a7c24c0cead5
Dawson MacPhee <dmacphee@uoguelph.ca>
2021-05-14 12:07:28 -0400

    Updated test package names

--- src/test/java/ca/FantasyHockeyTeamSelector/Website/WebsiteApplicationTests.java
+++ src/test/java/ca/FantasyHockeyTeamSelector/Website/WebsiteApplicationTests.java
@@ -1,10 +1,10 @@
-package ca.FantasyHockeyTeamSelector.WebController;
+package ca.FantasyHockeyTeamSelector.Website;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebControllerApplicationTests {
+class WebsiteApplicationTests {
 
 	@Test
 	void contextLoads() {
