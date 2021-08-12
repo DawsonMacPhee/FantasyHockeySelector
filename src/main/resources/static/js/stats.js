64608f1db72e1ba90ce66327830a346f4b57c0f1
git config --global user.name mack-anderson <mackanderson75@gmail.com>
2021-08-11 20:17:15 -0400

    added goalie stats table and stat selection

--- src/main/resources/static/js/stats.js
+++ src/main/resources/static/js/stats.js
@@ -58,7 +58,7 @@ testStat = {
             "hits": "100"
         },
         {
-            "playerId": "8479337",
+            "playerId": "8477934",
             "name": "Leon Draisaitl",
             "season": "20202021",
             "team": "EDM",
@@ -459,16 +459,292 @@ testStat = {
     ]
 }
 
+pointStats = {
+    "players": [
+        {
+            "playerId": "8478402",
+            "name": "Connor McDavid",
+            "season": "20202021",
+            "team": "EDM",
+            "position": "C",
+            "games": "56",
+            "goals": "33",
+            "assists": "72",
+            "points": "105",
+            "plusMinus": "21",
+            "pim": "20",
+            "shots": "200",
+            "shotPct": "16.5",
+            "timeOnIcePerGame": "22:09",
+            "faceOffPct": "49.5",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8477934",
+            "name": "Leon Draisaitl",
+            "season": "20202021",
+            "team": "EDM",
+            "position": "C",
+            "games": "56",
+            "goals": "31",
+            "assists": "53",
+            "points": "84",
+            "plusMinus": "29",
+            "pim": "22",
+            "shots": "168",
+            "shotPct": "18.5",
+            "timeOnIcePerGame": "22:11",
+            "faceOffPct": "55.7",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8473419",
+            "name": "Brad Marchand",
+            "season": "20202021",
+            "team": "BOS",
+            "position": "L",
+            "games": "53",
+            "goals": "29",
+            "assists": "40",
+            "points": "69",
+            "plusMinus": "26",
+            "pim": "46",
+            "shots": "143",
+            "shotPct": "20.3",
+            "timeOnIcePerGame": "18:55",
+            "faceOffPct": "31.4",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8479318",
+            "name": "Auston Matthews",
+            "season": "20202021",
+            "team": "TOR",
+            "position": "C",
+            "games": "52",
+            "goals": "41",
+            "assists": "25",
+            "points": "66",
+            "plusMinus": "21",
+            "pim": "10",
+            "shots": "222",
+            "shotPct": "18.5",
+            "timeOnIcePerGame": "21:33",
+            "faceOffPct": "51.9",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8478420",
+            "name": "Mikko Rantanen",
+            "season": "20202021",
+            "team": "COL",
+            "position": "R",
+            "games": "52",
+            "goals": "30",
+            "assists": "36",
+            "points": "66",
+            "plusMinus": "30",
+            "pim": "34",
+            "shots": "177",
+            "shotPct": "17",
+            "timeOnIcePerGame": "19:55",
+            "faceOffPct": "50",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8477493",
+            "name": "Aleksander Barkov",
+            "season": "20202021",
+            "team": "FLA",
+            "position": "C",
+            "games": "50",
+            "goals": "26",
+            "assists": "32",
+            "points": "58",
+            "plusMinus": "12",
+            "pim": "14",
+            "shots": "176",
+            "shotPct": "14.8",
+            "timeOnIcePerGame": "20:56",
+            "faceOffPct": "54.9",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8479337",
+            "name": "Alex DeBrincat",
+            "season": "20202021",
+            "team": "CHI",
+            "position": "L",
+            "games": "52",
+            "goals": "32",
+            "assists": "24",
+            "points": "56",
+            "plusMinus": "3",
+            "pim": "12",
+            "shots": "155",
+            "shotPct": "20.7",
+            "timeOnIcePerGame": "20:32",
+            "faceOffPct": "40",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8478864",
+            "name": "Kirill Kaprizov",
+            "season": "20202021",
+            "team": "MIN",
+            "position": "L",
+            "games": "55",
+            "goals": "27",
+            "assists": "24",
+            "points": "51",
+            "plusMinus": "10",
+            "pim": "16",
+            "shots": "157",
+            "shotPct": "17.2",
+            "timeOnIcePerGame": "18:18",
+            "faceOffPct": "12.5",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8470794",
+            "name": "Joe Pavelski",
+            "season": "20202021",
+            "team": "DAL",
+            "position": "C",
+            "games": "56",
+            "goals": "25",
+            "assists": "26",
+            "points": "51",
+            "plusMinus": "22",
+            "pim": "16",
+            "shots": "139",
+            "shotPct": "18",
+            "timeOnIcePerGame": "19:01",
+            "faceOffPct": "52",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8478398",
+            "name": "Kyle Connor",
+            "season": "20202021",
+            "team": "WPG",
+            "position": "L",
+            "games": "56",
+            "goals": "26",
+            "assists": "24",
+            "points": "50",
+            "plusMinus": "-4",
+            "pim": "12",
+            "shots": "166",
+            "shotPct": "15.7",
+            "timeOnIcePerGame": "18:44",
+            "faceOffPct": "60",
+            "blocked": "50",
+            "hits": "100"
+        },
+        {
+            "playerId": "8475726",
+            "name": "Tyler Toffoli",
+            "season": "20202021",
+            "team": "MTL",
+            "position": "R",
+            "games": "52",
+            "goals": "28",
+            "assists": "16",
+            "points": "44",
+            "plusMinus": "0",
+            "pim": "24",
+            "shots": "158",
+            "shotPct": "17.7",
+            "timeOnIcePerGame": "17:31",
+            "faceOffPct": "38.9",
+            "blocked": "50",
+            "hits": "100"
+        }
+    ]
+}
+
 $("document").ready( function () {
     playerStatTable(testStat);
+    createSeasonSelection();
+    $("#goalieTable").hide();
+    $("#goalieStats").hide();
+    
 }); 
+
+$('#p').click(function() {
+    playerStatTable(pointStats);
+});
+
+
+$('td').bind('mouseover mouseleave', function (e) {
+    var index = this.cellIndex;
+
+    if (e.type == "mouseover") {
+        $(this).parent().addClass('highlight');
+        $(this).parents('table').find('tr').each(function () {
+            $(this).find('td').eq(index).addClass('highlight');
+        });
+    } else {
+        $(this).parent().removeClass('highlight');
+        $(this).parents('table').find('tr').each(function () {
+            $(this).find('td').eq(index).removeClass('highlight');
+        });
+    }
+});
+
+function createSeasonSelection() {
+    element = "season";
+    var date = new Date();
+    var year = date.getFullYear();
+    if (date.getMonth() >= 9) {
+        for (let index = year; index >= year-5; index--) {
+            document.getElementById(element).innerHTML +=
+            `<option value="${index}${index+1}">${index}-${index+1}</option>`;
+        }
+    }
+    else{
+        for (let index = year-1; index >= year-5; index--) {
+            document.getElementById(element).innerHTML +=
+            `<option value="${index}${index+1}">${index}-${index+1}</option>`;
+        }
+    }
+}
+
+function gpSelection() {
+    
+    if (document.getElementById("gorp").value.localeCompare("player") != 0) {
+        $("#playerPosition").hide();
+        $("#playerStats").hide();
+        $("#goalieStats").show();
+    }
+    else{
+        $("#playerPosition").show();
+        $("#playerStats").show();
+        $("#goalieStats").hide();
+    }
+}
 
 /**
 * Add stat panels on right side of landing page
 * @param {JSON LIST of statistic leaders} leaderList 
 */
 function playerStatTable(leaderList) {
-    element = "playerTable";
+    element = "playerTableBody";
+    var parent = document.getElementById(element);
+    while(parent.hasChildNodes())
+    {
+        parent.removeChild(parent.firstChild);
+    }
     statList = ["name", "season", "team", "position", "games", "goals", "assists", "points", "plusMinus", "pim", "shots", "shotPct", "timeOnIcePerGame", "faceOffPct", "blocked", "hits"]; //list of stats
     for (let i in leaderList.players) { //create panel for each stat
         playerImg = "https://cms.nhl.bamgrid.com/images/headshots/current/168x168/" + leaderList.players[i].playerId + "@2x.jpg"; //store player img src
